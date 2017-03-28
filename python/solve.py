66ec20ab5a747b78fd87f88443eee722d20cf769
Joshua T Corbin <jcorbin@wunjo.org>
2017-03-28 09:47:26 -0700

    XXX first pass notes

--- python/solve.py
+++ python/solve.py
@@ -3,6 +3,7 @@
 import unittest
 import copy
 from simplejson import dumps  # for lazy deep equality testing
+# XXX why not just the normal json module? why 3rd party?
 
 # Sample Snake Ascii Art!
 # |X|X|X|
@@ -31,9 +32,33 @@ sample_snake_block_form = [3, 2, 1, 2, 1, 2, 3, 1, 2, 2, 1, 3, 1, 3]
 # a = adjacent
 # s = start
 sample_snake_adjacency_form = "sffaaffaffaaffafaaaffafafaf"
+# XXX it's unclear to me hat the rules of this form are, porting that string
+# back onto your above:
 
-# Cube representation - We need to store either an empty space or a "direction" for sections
-# of the snake that have no degrees of freedom (forced/ fixed sections).
+# |s|f|f|              } I can't seem to recover whatever rule you built that
+#     |a|a|            } string with by reading back from this diagram.
+#       |f|            } I've tried:
+#       |f|a|          } - "a"s indicate a vertical segment
+#         |f|          } - "a"s are the "mid section" of a vertical segment
+#         |f|a|        } - "a"s are the tail of a vertical segment
+#           |a|f|f|    }   - i.e. an "a" under an "f" or another "a"
+#               |a|    }
+#               |f|a|  }
+#                 |a|a|
+#                   |f|/------ XXX looks like an off by one section?
+#                   |f|a|f| /
+#                       |a|/
+#                       |f|a|f|
+
+# XXX until this point, given your diagram at top and the forms here, I had so
+# far thought "whew, okay looks like we'll be solving a reduced problem in 2d
+# rather than 3d; great! I was barely hanging on to the mental model in 2d so
+# far!"... but then the next section clearly says "cube", resurrecting my fear
+# response ;-)
+
+# Cube representation - We need to store either an empty space or a "direction"
+# for sections of the snake that have no degrees of freedom (forced/ fixed
+# sections).
 # Directional encoding scheme:
 #    E -> empty space
 #   xX -> -+ X axis
@@ -42,6 +67,7 @@ sample_snake_adjacency_form = "sffaaffaffaaffafaaaffafafaf"
 
 
 def init_cube():
+    # XXX I shed a tear for a missed opportunity for numpy ;-)
     return [
         [
             ['E' for _ in xrange(3)]
@@ -52,6 +78,8 @@ def init_cube():
 # Helper data structure maps directional encoding to xyz index
 
 
+# XXX unsure hat this even means, especiall ysince it seems that there's no
+# difference between littel and big?
 encoding = {
     'x': 0,
     'X': 0,
@@ -78,6 +106,9 @@ adjacent_moves = {
     'Z': adj_x + adj_y,
 }
 
+# XXX woof, I really want a visualization of what all that adj_{x,y,z} and
+# adjacent_moves even means; barely hanging on...
+
 # Queue for next move to search
 moves = []
 solved = False
@@ -90,17 +121,28 @@ snake = ""
 
 
 def solve(_snake):
+    # XXX oh dear, so it does look like the representation we went with is the
+    # adjacency string that I had so much troule grokking above, rather than
+    # the block form... #strapin #holdon #stepbackimgoingtoscience
     if len(_snake) != 27 or _snake[0] != "s":
         return "Invalid"
     global moves, snake
+    # XXX a tear is shed for a class that could've been (and helped to define
+    # "scope" for the reader)
     snake = _snake
     cube = init_cube()
     moves += generate_starting_moves(cube)
     while len(moves) > 0 and not solved:
         curMove = moves[0]
-        # Dequeue
+        # Dequeue XXX indeed, you probably should've used one ;-)
         moves = moves[1:]
         moves += generate_next_move(curMove)
+    # XXX oh I get it now! This is a search algorithm:
+    # - moves is your frontier
+    # - generate_starting_moves seeds it
+    # - generate_next_move is your successor function
+    # - solved wants to be a goal function
+    # ... I may not grok the 3-space part of it, but now we're talking!
     if solved:
         return "Solveable!"
     else:
@@ -114,6 +156,20 @@ def solve(_snake):
 # - (0,0,0) for the corner
 # - (0,0,1) for the middle edge
 # - (1,0,1) for the "face center"
+
+# XXX okay great, but I'm not really sure what the components in those tuples
+# even are? Maybe they're just notional, but there's some reason you have 3
+# components, and made those choices for middle edge and face center. Anyhow,
+# I'm pretty sure my continuing difficulty stems from an utter lack of
+# intuition for the actual domain being modeled here.
+#   XXX after the fact: finally started to get a handle on what these are when
+#   reading your successor gunction (aka generate_next_move).
+#   I think:
+#   - they're just points into your cube?
+#   - my current working guess for "the shape of your search state" is:
+#     - a cube being filled in...
+#     - ... a cursor in the input representation
+#     - ... a list of moves (altho I'm not yet sure what a "move" is...)
 
 
 def generate_starting_moves(cube):
@@ -144,11 +200,14 @@ def add_to_cube(cube, position, direction):
 
 
 def generate_next_move(curMove):
+    # XXX and here is the first time I've really understood the structure of
+    # what a move is; declaring this data type first, e.g. using a namedtuple,
+    # would've helped immensely
     cube, snake_index, position = curMove
     print snake_index, len(snake)
     if snake_index == len(snake):
         global solved
-        solved = True
+        solved = True  # XXX this wants to be some `func goal(state T) bool`
         return []
     x, y, z = position
     direction = cube[x][y][z]
